55b0737f41ccfe493b80afd05904c18dc90b92c8
Pallav Dubey <pallav.dubey@gmail.com>
2022-03-10 09:57:20 -0500

    update connection string

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -1,5 +1,5 @@
 <?php
 
-$connect = mysqli_connect( "localhost", "root", "root", "" );
+$connect = mysqli_connect("localhost", "root", "7iudiDK9j53mSm", "cms");
 
 mysqli_set_charset( $connect, 'UTF8' );
